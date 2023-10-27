977fa4909fb35f31bba6c52f5d6e0e9a7a333aea
muhammedunaispk <pkmuhammedunais@gmail.com>
2023-10-27 09:15:18 +0530

    Kitchen_Items

--- main.js
+++ main.js
@@ -5,48 +5,65 @@ let kitchenItemslist = document.getElementById("kitchen-items-list");
 // step 2
 // add kitchen items
 function addKitchenItems() {
-    let kitchenInputData = kitchenInput.value;
-    // Create DOM elements now
-    let li = document.createElement("li");
-    li.innerText = kitchenInputData;
-    li.style.cssText = "animation-name: slideIn;";
-    kitchenItemslist.appendChild(li);
-    kitchenInput.value = "";
-    kitchenInput.focus();
-    // Create trash butoon
-    let trashBtn = document.createElement("i");
-    trashBtn.classList.add("fa", "fa-trash");
-    li.appendChild(trashBtn);
+  let kitchenInputData = kitchenInput.value;
+  // Create DOM elements now
+  let li = document.createElement("li");
+
+  let spanEl = document.createElement("span");
+  li.appendChild(spanEl);
+  spanEl.innerText = kitchenInputData;
+
+  li.style.cssText = "animation-name: slideIn;";
+  kitchenItemslist.appendChild(li);
+  kitchenInput.value = "";
+  kitchenInput.focus();
+  // Create trash butoon
+  let trashBtn = document.createElement("i");
+  trashBtn.classList.add("fa", "fa-trash");
+  li.appendChild(trashBtn);
+
+  let editBtn = document.createElement("i");
+  editBtn.classList.add("fas", "fa-edit");
+  li.appendChild(editBtn);
 }
 
 // Delete item from kitchen list
-function deleteKitchenItem(event){
-    console.log(event.target.classList[0]);
-    if(event.target.classList[0] === "fa") {  // event.target is "i"
+function deleteKitchenItem(event) {
+  if (event.target.classList[1] === "fa-trash") {  // event.target is "i"
+    let item = event.target.parentElement;
+    item.classList.add("slideOut");
+    item.addEventListener("transitionend", function () {
+      item.remove();
+    })
+    // item.remove();
+  }
+}
+
+// Edit Kitchen Item
+function editKitchenItem(event) {
+    if (event.target.classList[1] === "fa-edit") {  // event.target is "i"
+      let editValue = prompt("Please add new text");
       let item = event.target.parentElement;
-      item.classList.add("slideOut");
-      item.addEventListener("transitionend", function () {
-        item.remove();
-      })
-      // item.remove();
+      let spanEl = item.querySelector("span");
+      spanEl.innerText = editValue;
+      console.log(spanEl);
     }
 }
 
+// Enter key for add items
+kitchenInput.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("add-btn").click();
+  }
+})
+
 // step 1
 // add an event listener to the button
-addBtn.addEventListener("click", addKitchenItems)
+addBtn.addEventListener("click", addKitchenItems);
+kitchenItemslist.addEventListener("click", deleteKitchenItem);
+kitchenItemslist.addEventListener("click", editKitchenItem);
 
-
-// Enter key for add items
-kitchenInput.addEventListener("keypress", function(event) {
-    // If the user presses the "Enter" key on the keyboard
-    if (event.key === "Enter") {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      document.getElementById("add-btn").click();
-    }
-  }) 
-
-
-  kitchenItemslist.addEventListener("click", deleteKitchenItem);
